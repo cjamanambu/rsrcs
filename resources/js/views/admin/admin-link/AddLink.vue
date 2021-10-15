87be2dd5d4295d0e18dac45e82943bf37ae027e9
CJ Amanambu <oamanambu@yahoo.com>
2021-10-15 10:49:28 +0100

    adds the add link functionality to the app

--- resources/js/views/admin/admin-link/AddLink.vue
+++ resources/js/views/admin/admin-link/AddLink.vue
@@ -1,7 +1,89 @@
 <template>
-  <div></div>
+  <div>
+    <main role="main">
+      <PageHeader page-title="Add Link Resource" :crumbs="crumbs" />
+      <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2">
+        <p class="text-muted">Here you can add a new Link resource</p>
+        <button class="btn btn-secondary btn-sm" @click="$router.push({ name: 'admin-link'})">Go Back</button>
+      </div>
+      <div class="card">
+        <form @submit.prevent="addLink">
+          <div class="card-body">
+            <h5 class="card-title mb-4">Add Link Form</h5>
+            <div class="form-group mb-4">
+              <label for="title">Link Title</label><span class="text-danger"> *</span>
+              <input type="text" class="form-control" id="title" v-model="link.title" required>
+            </div>
+            <div class="form-group mb-4">
+              <label for="url">Link URL</label><span class="text-danger"> *</span>
+              <input type="url" class="form-control" id="url" v-model="link.url" placeholder="http://localhost.com" required>
+            </div>
+            <div class="form-group mb-4">
+              <div class="custom-control custom-checkbox">
+                <input type="checkbox" class="custom-control-input" id="new-tab" v-model="link.newTab">
+                <label class="custom-control-label" for="new-tab">Check this to open this link in a new tab</label>
+              </div>
+            </div>
+            <button v-if="!loading" class="btn btn-primary">Add Link</button>
+            <button v-if="loading" class="btn btn-primary" disabled>Adding... </button>
+          </div>
+        </form>
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
-export default {}
+import PageHeader from '../components/PageHeader.vue'
+export default {
+  components: { PageHeader },
+  data() {
+    return {
+      loading: false,
+      link: {},
+      crumbs: [
+        { id: 1, name: 'Dashboard', active: false, path: '/admin' },
+        { id: 2, name: 'Link Resource', active: false, path: '/admin/link' },
+        { id: 3, name: 'Add Link Resource', active: true, path: '' },
+      ],
+    }
+  },
+  methods: {
+    addLink() {
+      this.$swal({
+        title: 'Are you sure?',
+        text: `This will add the link resource: ${this.link.title}`,
+        type: 'warning',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, add it!'
+      }).then(result => {
+        if (result.value) {
+          this.loading = true
+          const formData = new FormData()
+          formData.append('title', this.link.title)
+          formData.append('link', this.link.url)
+          formData.append('new_tab', this.link.newTab ? '1' : '0')
+          this.axios.post('http://localhost:8000/api/admin/link/add', formData).then(response => {
+            this.$router.push({ name: 'admin-link'}).then(() => {
+              this.$swal({
+                title: 'Success',
+                text: response.data,
+                icon: 'success'
+              })
+            })
+          }).catch(error => {
+            this.$swal({
+              title: 'Error',
+              text: error.response.data.message,
+              icon: 'error'
+            })
+          }).finally(() => this.loading = false)
+        }
+      })
+    }
+  }
+}
 </script>
